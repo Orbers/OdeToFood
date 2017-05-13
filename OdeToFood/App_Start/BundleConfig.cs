e52aef4589c69e8315b7d1dccfbdd7ca183d0538
corbin <theseegsjnr@gmail.com>
2017-05-13 07:58:42 -0500

    AJAX Helpers, 2min in, currently works

--- OdeToFood/App_Start/BundleConfig.cs
+++ OdeToFood/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace OdeToFood
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css")); 
+                      "~/Content/Site.css")); 
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
